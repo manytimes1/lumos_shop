be3d9066a7039827bce469aae574f811f3fd6faa
Ivo <frostwolfe91@gmail.com>
2019-08-17 13:01:05 +0300

    Update CSS/JS files and make them ready to compile

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+import '../scss/style.scss';
+import '../scss/colors/megna-dark.scss';
+
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import 'jquery-slimscroll'
+import './sidebarmenu.js'
+import 'sticky-kit/dist/sticky-kit'
+import './custom.min.js'
+import './confirmation_modal.js'
+import '../plugins/datatables.net/js/jquery.dataTables.min'
+import '../plugins/datatables.net-bs4/js/dataTables.responsive.min'
+import './datatables';
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,12 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
     .setOutputPath('public/')
-    .setPublicPath('/public')
+    .setPublicPath('/')
     .cleanupOutputBeforeBuild()
     .autoProvidejQuery()
     .enableSassLoader()
-    // when versioning is enabled, each filename will include a hash that changes
-    // whenever the contents of that file change. This allows you to use aggressive
-    // caching strategies. Use Encore.isProduction() to enable it only for production.
-    .enableVersioning(false)
-    .addEntry('/js/app', './assets/js/app.js')
-    .addStyleEntry('/css/style', './assets/scss/style.scss')
-    .addStyleEntry('/css/colors/megna-dark', './assets/scss/colors/megna-dark.scss')
+    .enableVersioning(Encore.isProduction())
+    .addEntry('app', './assets/js/app.js')
     .splitEntryChunks()
     .enableSingleRuntimeChunk()
     .enableIntegrityHashes(Encore.isProduction())
