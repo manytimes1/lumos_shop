97ae59bda06c812bb81bcb08e9b7e85af46a3f51
Ivo <frostwolfe91@gmail.com>
2019-08-24 13:35:16 +0300

    Add customer functionality

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -3,11 +3,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Profile;
 use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\RoleRepository;
 use App\Repository\UserRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class CustomerController extends AbstractController
 {
@@ -27,9 +32,111 @@ class CustomerController extends AbstractController
         }
 
         $customers = $userRepository->findAllCustomersByRole();
+        $form = $this->createForm(UserType::class);
 
         return $this->render('customer/list.html.twig', [
-            'customers' => $customers
+            'customers' => $customers,
+            'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/customer/add", name="add_new_customer")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param RoleRepository $roleRepository
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function addNew(Request $request,
+                           UserPasswordEncoderInterface $passwordEncoder,
+                           RoleRepository $roleRepository)
+    {
+        $currentUser = $this->getUser();
+
+        if (!$currentUser->isAdmin()) {
+            return $this->redirectToRoute('index');
+        }
+
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        $user->setPassword(
+            $passwordEncoder->encodePassword(
+                $user,
+                $form->get('plainPassword')->getData()
+            )
+        );
+
+        $role = $roleRepository->findOneBy(['name' => 'ROLE_USER']);
+        $user->addRole($role);
+
+        $profile = new Profile();
+        $profile->setFirstName($user->getProfile()->getFirstName());
+        $profile->setLastName($user->getProfile()->getLastName());
+        $profile->setPhone($user->getProfile()->getPhone());
+        $profile->setLocation($user->getProfile()->getLocation());
+        $profile->setCash(1000);
+        $profile->setUser($user);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($profile);
+
+        $user->setProfile($profile);
+        $user->setStatus(1);
+        $em->persist($user);
+        $em->flush();
+
+        $this->addFlash('success', 'Customer added successfully.');
+
+        return $this->redirectToRoute('list_all_customers');
+    }
+
+    /**
+     * @Route("/customer/{id}", name="customer_detail")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     * @param UserRepository $userRepository
+     * @param User $user
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show(UserRepository $userRepository, User $user)
+    {
+        $currentUser = $this->getUser();
+
+        if (!$currentUser->isAdmin()) {
+            return $this->redirectToRoute('index');
+        }
+
+        $customer = $userRepository->find($user);
+        $form = $this->createForm(UserType::class, $user);
+
+        return $this->render('customer/show.html.twig', [
+            'customer' => $customer,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/customer/delete/{id}", name="customer_delete")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     * @param User $user
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function remove(User $user)
+    {
+        $currentUser = $this->getUser();
+
+        if (!$currentUser->isAdmin()) {
+            return $this->redirectToRoute('index');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        $this->addFlash('success', 'Customer deleted successfully.');
+
+        return $this->redirectToRoute('list_all_customers');
     }
 }
