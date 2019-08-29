53ab238f8614ba7b7465ccb965749e4d74bb71de
Ivo <frostwolfe91@gmail.com>
2019-08-29 16:42:40 +0300

    * Feature: Pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,25 +2,27 @@
 
 namespace App\Controller;
 
+
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", defaults={"page": "1"}, name="index")
+     * @Route("/page/{page<[1-9]\d*>}", methods={"GET"}, name="index_paginated")
+     * @Cache(smaxage="10")
      */
-    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository)
+    public function listAll(int $page, ProductRepository $productRepository,
+                          CategoryRepository $categoryRepository)
     {
-        $category = $categoryRepository->findAll();
-        $products = $productRepository->findBy([
-            'category' => $category
-        ]);
+        $latestProducts = $productRepository->findLatest($page);
 
         return $this->render('home/index.html.twig', [
-            'products' => $products
+            'paginator' => $latestProducts,
         ]);
     }
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Product;
+use App\Pagination\Paginator;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -11,5 +12,17 @@ class ProductRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    public function findLatest(int $page): Paginator
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->addSelect('c')
+            ->innerJoin('p.category', 'c')
+            ->where('p.addedOn <= :now')
+            ->orderBy('p.addedOn', 'DESC')
+            ->setParameter('now', new \DateTime());
+
+        return (new Paginator($qb))->paginate($page);
     }
 }
